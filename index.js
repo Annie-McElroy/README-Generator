8f05c5b2394ace09c9baad926e074ad628388b55
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-07-21 21:58:33 -0500

    Added questions array of objects

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is your description of your project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions for your application?',
+        name: 'installation instructions',
+    },
+    {
+        type: 'input',
+        message: 'How should a user use your application?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What are your guidelines for contribution to your project?',
+        name: 'constributions',
+    },
+    {
+        type: 'input',
+        message: 'What instructions do you have for testing?',
+        name: 'testing instructions',
+    },
+    {
+        type: 'list',
+        message: 'What license do you have for your project?',
+        name: 'license',
+        choices: ['MIT', 'GNU', 'Apache', 'Mozilla']
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
